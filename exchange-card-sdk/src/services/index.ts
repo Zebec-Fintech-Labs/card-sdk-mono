b37e832ec277f074fb173cdeb74c4e692006251f
Ashish Sapkota <aacsspkt3@gmail.com>
2025-05-09 12:37:45 +0545

    refactor: remove unused service classes

--- exchange-card-sdk/src/services/index.ts
+++ exchange-card-sdk/src/services/index.ts
@@ -1,7 +1,3 @@
 // export * from "./bitcoinService";
-// export * from "./EvmService";
 export * from "./stellarService";
-export * from "./TaoService";
-export * from "./TonService";
-export * from "./XdbService";
 export * from "./xrplService";
