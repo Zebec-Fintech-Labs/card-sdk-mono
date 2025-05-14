ed7b8466b43ccef6a6194deeed9c04beb6471aed
Ashish Sapkota <aacsspkt3@gmail.com>
2025-05-14 15:39:20 +0545

    feat: add transaction fetching and assertion in XRPL service tests and test createTrustLine methods for transfer

--- exchange-card-sdk/test/xrplService.test.ts
+++ exchange-card-sdk/test/xrplService.test.ts
@@ -1,5 +1,6 @@
+import assert from "assert";
 import { describe, it } from "mocha";
-import { SubmittableTransaction } from "xrpl";
+import { Client, Payment, SubmittableTransaction, TxResponse } from "xrpl";
 
 import { XRPLService } from "../src";
 import { getXRPLWallet } from "./setup";
@@ -18,7 +19,7 @@ const service = new XRPLService(
 		encryptionKey: process.env.ENCRYPTION_KEY!,
 	},
 	{
-		sandbox: false,
+		sandbox: true,
 	},
 );
 
@@ -36,20 +37,29 @@ describe("XRPL Service", () => {
 		});
 	});
 
-	describe("getDecimalsOfCurrency", () => {
-		it("getDecimalsOfCurrency", async () => {
-			const account = "rBwV8RZ3CTC2bvRvJTo9Yj55njd1woFW1W";
-			const currency = "XRH";
-			const decimals = await service.getDecimalsOfCurrency(account, currency);
-			console.log("decimals:", decimals);
+	describe("transferRLUSD", () => {
+		it("transferRLUSD", async () => {
+			const params = {
+				amount: "0.1",
+				token: {
+					currency: "524C555344000000000000000000000000000000", // RLUSD
+					issuer: "rQhWct2fv4Vc4KRjRgMrxa8xPN9Zx9iLKV", // RLUSD issuer
+				},
+				walletAddress: wallet.address,
+			};
 
+			const response = await service.createTrustLine(params);
+			console.log("response:", JSON.stringify(response, null, 2));
+
+			const response1 = await service.transferTokens(params);
+			console.log("response:", JSON.stringify(response1, null, 2));
 			// assert(response, "Response should not be null");
 		});
 	});
 
 	describe("getTokenBalances", () => {
 		it("getTokenBalances", async () => {
-			const account = "rBwV8RZ3CTC2bvRvJTo9Yj55njd1woFW1W";
+			const account = wallet.address;
 			const balances = await service.getTokenBalances(account);
 			console.log("balances:", JSON.stringify(balances, null, 2));
 
@@ -65,6 +75,37 @@ describe("XRPL Service", () => {
 			vaultAddress = await service.fetchVault("RLUSD");
 			console.log("vaultAddress:", vaultAddress);
 			// assert(response, "Response should not be null");
+		});
+	});
+
+	describe("fetch xrpl transaction", () => {
+		it("fetch transaction", async () => {
+			const client = new Client("wss://s.altnet.rippletest.net:51233");
+
+			await client.connect();
+
+			try {
+				const tx: TxResponse = await client.request({
+					command: "tx",
+					transaction: "D5C3D63A8A2D04B82B3CAB26CBBFE49EA16D1FF17A31B2824BF32884F464D35C",
+				});
+				const transaction = tx.result.tx_json as Payment;
+				console.log("Transaction:", JSON.stringify(transaction, null, 2));
+
+				assert("DeliverMax" in transaction, "Transaction should have DeliverMax");
+
+				const destination = transaction.Destination;
+				const account = transaction.Account;
+				const amount = transaction.DeliverMax;
+
+				console.log("account:", account);
+				console.log("destination:", destination);
+				console.log("amount:", amount);
+			} catch (error) {
+				throw error;
+			} finally {
+				await client.disconnect();
+			}
 		});
 	});
 });
