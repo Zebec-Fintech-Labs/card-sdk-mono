beb7096c6c0699afdf81a15c6449e6f43838b1f4
Ashish Sapkota <ashishspkt6566@gmail.com>
2025-07-16 13:27:50 +0545

    test: update AlgorandWallet to use signAndSendTransaction method for transaction handling

--- exchange-card-sdk/test/algorandService.test.ts
+++ exchange-card-sdk/test/algorandService.test.ts
@@ -1,14 +1,32 @@
+import algosdk from "algosdk";
+
+import { ClientManager } from "@algorandfoundation/algokit-utils/types/client-manager";
+
 import { AlgorandService, AlgorandWallet } from "../src";
 import { getAlgorandAccount } from "./setup";
 
 const senderAccount = getAlgorandAccount();
 
 console.log("Sender Account:", senderAccount.addr.toString());
-
 const algorandWallet: AlgorandWallet = {
 	address: senderAccount.addr,
-	signTransaction: async (txn) => {
-		return txn.signTxn(senderAccount.sk);
+	signAndSendTransaction: async (txn) => {
+		const signedTxn = txn.signTxn(senderAccount.sk);
+
+		const algodClient = ClientManager.getAlgodClient(
+			ClientManager.getAlgoNodeConfig("testnet", "algod"),
+		);
+		// Submit the transaction
+		const txResponse = await algodClient.sendRawTransaction(signedTxn).do();
+
+		// Wait for confirmation
+		await algosdk.waitForConfirmation(
+			algodClient,
+			txResponse.txid,
+			4, // Wait for 4 rounds
+		);
+
+		return txResponse.txid;
 	},
 };
 
