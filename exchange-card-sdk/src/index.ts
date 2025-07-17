16a6086c487d6bd27b33615535d1b3c377b6f12a
Ashish Sapkota <ashishspkt6566@gmail.com>
2025-07-17 21:05:46 +0545

    refactor: remove unused utility and features

--- exchange-card-sdk/src/index.ts
+++ exchange-card-sdk/src/index.ts
@@ -1,7 +1,5 @@
 export * from "./artifacts";
-export * from "./chains";
 export * from "./constants";
-export * from "./errors";
 export * from "./services";
 export * from "./types";
 export * from "./utils";
